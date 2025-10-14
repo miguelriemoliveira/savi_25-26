2493b83f6ac6508f03870e73eceaf3d9bbf4b9f7
Miguel Oliveira <m.riem.oliveira@gmail.com>
2025-10-14 11:26:59 +0100

    ...

--- Parte05/Ex3/main.py
+++ Parte05/Ex3/main.py
@@ -7,81 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import json
+from scipy.optimize import least_squares
+from line_model import LineModel
 
 import matplotlib
 matplotlib.use('QtAgg')
 # Select one option that works for your OS
 # https://matplotlib.org/stable/users/explain/figure/backends.html
-
-
-class LineModel:
-
-    def __init__(self, m=None, b=None):
-
-        self.m = m
-        self.b = b
-        self.plot_handle = None
-
-        # Randomize if m and b are not defined
-        if self.m is None or self.b is None:
-            self.randomizeParameters()
-
-        print('Created a line model:\n' + self.__str__())
-
-    def randomizeParameters(self):
-        self.m = random.uniform(-2, 2)
-        self.b = random.uniform(-10, 10)
-
-    def draw(self, color='b'):
-
-        # linspace_example = np.linspace(-10, 10, 100)
-
-        xs = [-10, 10]
-        ys = self.getYs(xs)
-
-        if self.plot_handle is None:
-            # plot the first time and get the drawing handle
-            self.plot_handle = plt.plot(xs, ys, '-' + color, linewidth=2)
-        else:
-            plt.setp(self.plot_handle, xdata=xs, ydata=ys)
-
-    def getYs(self, xs):
-        ys = []
-        for x in xs:
-            y = self.m * x + self.b  # apply the line equation
-            ys.append(y)  # appens to the list
-
-        return ys
-
-    def getError(self, xs_gt, ys_gt):
-
-        ys = self.getYs(xs_gt)  # usign my model, compute the y coordinates for the gt_xs
-
-        # Computing the errors
-        errors = []
-        for y, y_gt in zip(ys, ys_gt):
-            error = abs(y_gt - y)
-            errors.append(error)
-
-        # Compute the average error
-        n = len(xs_gt)
-        total = 0
-        for error in errors:
-            total += error
-
-        average_error = total / n
-        return average_error
-
-    def __str__(self):
-        return "Line m=" + str(self.m) + ' b=' + str(self.b)
-
-# How to use the class
-# it will set the values of self.m and self.b from the values of m and b
-# line2 = LineModel(m=3, b=5)  # named arguments -> define the name order is not importante
-# line2 = LineModel(b=5, m=3)  # named arguments -> define the name order is not importante
-# unnamed arguments -> will use the order oif th arguments to atribute values
-# line2 = LineModel(3, 5)
-# line3 = LineModel(b=3)
 
 
 def main():
@@ -119,39 +51,42 @@ def main():
     # ------------------------------------
     # Create the line model
     # ------------------------------------
-
     line_model = LineModel()  # it will randomize m and b params
 
     # ------------------------------------
     # Optimization
     # ------------------------------------
 
-    plt.draw()  # draw in a non blocking fashion
+    # Define the objective function
+    def objectiveFunction(params):
 
-    minimum_error = None
-    best_line = LineModel()
+        # Extract the parameters to class properties
+        # We will define a convention in which
+        # params = [m , b]
+        line_model.m = params[0]
+        line_model.b = params[1]
 
-    for i in range(0, args['number_iterations']):
-
+        # Compute the error
         error = line_model.getError(xs_gt=xs_gt, ys_gt=ys_gt)
-        print('Line ' + str(i) + ' error = ' + str(error))
+        print('error = ' + str(error))
 
-        # Check if this current line is the best line
-        if minimum_error is None or error < minimum_error:
-            # store the parameters of the current line as the best line
-            best_line.m = line_model.m
-            best_line.b = line_model.b
-            minimum_error = error
-
-        line_model.draw()  # draw the line
-        best_line.draw(color='r')  # draw the best line in red
+        # Draw the new line
+        line_model.draw()
         plt.draw()  # draw in a non blocking fashion
         plt.waitforbuttonpress(0.1)  # wait for a second
 
-        line_model.randomizeParameters()  # Change the line's parameters
+        return error  # can be a scalar or a list
 
+    # start optimization
+    result = least_squares(objectiveFunction, [line_model.m, line_model.b])
+
+    print('Raw result of leas squares = ' + str(result))
+
+    print('Solution: m=' + str(result.x[0]) + ' b=' + str(result.x[1]))
     plt.show()  # keep the window open when the program finishes
 
 
 if __name__ == '__main__':
     main()
+
+y = a(x-h) ^ 2 + k
