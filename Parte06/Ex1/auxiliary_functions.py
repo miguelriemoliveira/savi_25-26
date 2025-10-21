f151b9f3286ad6119e4f5afdf6577d15faf6c62d
Miguel Oliveira <m.riem.oliveira@gmail.com>
2025-10-21 10:37:08 +0100

    ...

--- Parte06/Ex1/auxiliary_functions.py
+++ Parte06/Ex1/auxiliary_functions.py
@@ -4,6 +4,7 @@
 from copy import deepcopy
 import glob
 from random import randint
+import random
 import cv2  # import the opencv library
 # from matplotlib import pyplot as plt
 import numpy as np
@@ -11,3 +12,58 @@ import argparse
 
 # import matplotlib
 # matplotlib.use('Agg')
+
+
+def changeImageColor(image_in, s, b, mask=None):
+
+    # image_in is uint8 -> [0 (black), 255 (white)]
+    # [0, 255] * 1.0 -> float image
+
+    # Convert the image to float type
+    image_in_float = image_in.astype(float)  # 0 = black, 1=white
+
+    image_out_float = image_in_float * s + b  # apply model
+
+    image_out_float[image_out_float > 255] = 255  # saturate to 255
+    image_out_float[image_out_float < 0] = 0  # undersaturate to 0
+
+    # b = 0 255
+
+    # convert back to uint8
+    image_out = (image_out_float).astype(np.uint8)
+
+    # Copy back the original values of image_in in the regions where the mask is zero
+    if mask is not None:
+        mask_negated = np.logical_not(mask)
+        image_out[mask_negated] = image_in[mask_negated]
+
+    return image_out
+
+
+# Define the objective function
+def objectiveFunction(params, shared_mem):
+    # minimuzed version = objectiveFunction(params), shared_mem was already given
+
+    # Extract the parameters
+    # params = [s , b]
+    s = params[0]
+    b = params[1]
+    print('s = ' + str(s))
+    print('b = ' + str(b))
+    q_image = shared_mem['q_image']
+
+    # Applying the image model
+    q_image_out = changeImageColor(q_image, s=s, b=b, mask=shared_mem['q_mask'])
+
+    # Compute the error
+    # TODO how can we do this?
+    error = random.random()
+
+    # Draw the new line
+    win_name = 'query image'
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(win_name, q_image_out)  # type: ignore
+    cv2.waitKey(50)
+
+    print('error = ' + str(error))
+    return error  # can be a scalar or a list
--- Parte06/Ex1/main.py
+++ Parte06/Ex1/main.py
@@ -2,12 +2,15 @@
 # shebang line for linux / mac
 
 from copy import deepcopy
+from functools import partial
 import glob
 from random import randint
 import cv2  # import the opencv library
 # from matplotlib import pyplot as plt
 import numpy as np
 import argparse
+from auxiliary_functions import changeImageColor, objectiveFunction
+from scipy.optimize import least_squares
 
 # import matplotlib
 # matplotlib.use('Agg')
@@ -32,22 +35,35 @@ def main():
     # ----------------------------------
     # Load the query_transformed, query mask and target images stored in this folder
     # ----------------------------------
-    q_image_transformed = cv2.imread('q_image_transformed.png')
+    q_image = cv2.imread('q_image_transformed.png')
     t_image = cv2.imread('t_image.png')
     q_mask = cv2.imread('q_mask.png')
     # to have a dtype = boll so we can use as mask, e.g. image[q_mask]
     q_mask = (q_mask/255).astype(bool)
+
+    # test my functions
+
+    # q_image_changed = changeImageColor(q_image, s=2.5, b=30, mask=q_mask)  # type: ignore
+
+    # win_name = 'query image original'
+    # cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    # cv2.imshow(win_name, q_image)  # type: ignore
+
+    # win_name = 'query image changed'
+    # cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    # cv2.imshow(win_name, q_image_changed)  # type: ignore
+    # cv2.waitKey(0)
 
     # ------------------------------
     # Fusion or stitching
     # ------------------------------
 
     # Create a mask to denote where the q_image exists (is not black)
-    q_image_transformed_gray = cv2.cvtColor(q_image_transformed, cv2.COLOR_BGR2GRAY)  # type: ignore
+    q_image_transformed_gray = cv2.cvtColor(q_image, cv2.COLOR_BGR2GRAY)  # type: ignore
     q_mask = np.logical_not(q_image_transformed_gray == 0)
 
     mosaic_image = t_image  # the outer part is alreay ok, jsut need to change the middel
-    mosaic_image[q_mask] = 0.5 * t_image[q_mask] + 0.5 * q_image_transformed[q_mask]
+    mosaic_image[q_mask] = 0.5 * t_image[q_mask] + 0.5 * q_image[q_mask]
     # mosaic_image[q_mask] = q_image_transformed[q_mask]
 
     # Convert the mosaic back to unsigned integer 8 bits (uint8)
@@ -59,23 +75,26 @@ def main():
     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
     cv2.imshow(win_name, t_image)  # type: ignore
 
-    win_name = 'query image transformed'
-    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(win_name, q_image_transformed)  # type: ignore
+    # win_name = 'query mask'
+    # cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    # cv2.imshow(win_name, q_mask.astype(np.uint8)*255)  # type: ignore
 
-    win_name = 'query mask'
-    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(win_name, q_mask.astype(np.uint8)*255)  # type: ignore
+    # win_name = 'mosaic_image'
+    # cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    # cv2.imshow(win_name, mosaic_image)  # type: ignore
 
-    win_name = 'mosaic_image'
-    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(win_name, mosaic_image)  # type: ignore
+    # ------------------------------------
+    # Start optimization
+    # -------------------------------------
+    shared_mem = {'q_image': q_image, 't_image': t_image, 'q_mask': q_mask}
+
+    initial_params = [1.0, 0.0]
+    result = least_squares(partial(objectiveFunction, shared_mem=shared_mem),
+                           initial_params)
+
+    print('Optimization finished. Result=\n' + str(result))
 
     cv2.waitKey(0)
-    #
-    # ------------------------------------
-    # Open the video file
-    # -------------------------------------
 
 
 if __name__ == '__main__':
